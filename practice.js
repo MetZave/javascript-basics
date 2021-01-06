a9071771dcd408122d2b29d58bafc282acd6e485
Zack Metiva <zmmetiva@gmail.com>
2021-01-06 17:50:28 -0500

    Solution

--- practice.js
+++ practice.js
@@ -3,31 +3,31 @@
 //Create a variable called myName that is a string data type
 
 //Code here
-
+const myName = `Zack`
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
 //Code here
-
+const myAge = 23
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
 //Code here
-
+const lovesCode = true
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
 //Code here
-
+const greatestFear = undefined
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
 //Code here
-
+let devMountainGoal = null
 //////////////////PROBLEM 6////////////////////
 
 //Create a function declaration called greeting that
@@ -36,6 +36,11 @@
 //plus the value of the name parameter.
 
 //Code here
+function greeting(name) {
+  return `Hello, ${name}`
+}
+
+
 
 //////////////////PROBLEM 7////////////////////
 
@@ -44,13 +49,19 @@
 
 //Code Here
 
+
+const newGreeting = function(name) {
+  return `Hello, ${name}`
+}
+
+
 //////////////////PROBLEM 8////////////////////
 
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
 //Code Here
-
+let groceries = [`apples`, `milk`, `eggs`, `bread`]
 //////////////////PROBLEM 9////////////////////
 
 //Create an object saved to the variable dog.
@@ -59,12 +70,17 @@
 //and goodBoy (a boolean).
 
 //Code Here
-
+const dog = {
+  name: `George`,
+  color: `white`,
+  age: 2,
+  goodBoy: true
+}
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
 
 //Code Here
-
+let devMountainClassPet = dog.name
 //////////////////PROBLEM 10////////////////////
 
 //Write a function called nameCheck that takes in a name parameter.
@@ -76,6 +92,16 @@
 
 // Code here
 
+const nameCheck = NAMEPARAM => {
+  if(NAMEPARAM === `Steven`) {
+    return `What is up Steven?`
+  }
+  if(NAMEPARAM === `Bryan`) {
+    return `Hey Bryan!`
+  } else {
+    return `Cool name, ${NAMEPARAM}`
+  }
+}
 //////////////////PROBLEM 11////////////////////
 
 // Create a function called add that takes in two parameters
@@ -83,12 +109,14 @@
 // The add function should return the two parameters added together
 
 //Code Here
+const add = (numberone, numbertwo) => numberone + numbertwo
+
 
 //Now invoke add, passing in the numbers 3 and 4
 //storing the result in the variable mathSum.
 
 //Code Here
-
+let mathSum = add(3, 4)
 //////////////////PROBLEM 12////////////////////
 
 //Write a function called faveColorFinder that takes in one parameter called color
@@ -99,6 +127,20 @@
 // Otherwise, you should return the string 'you need to evaluate your favorite color choice'
 
 // Code here
+const faveColorFinder = color => {
+  if(color ===`red`) {
+    return `red is a great color`
+  }
+  if(color === `green`) {
+    return `green is a solid favorite color`
+  }
+  if(color === `black`) {
+    return `so trendy`
+  } else {
+    return `you need to evaluate your favorite color choice`
+  }
+}
+
 
 //////////////////PROBLEM 13////////////////////
 
@@ -122,28 +164,29 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 14////////////////////
 
 //Create a variable called age with your age assigned to you
 
 // Code Here
-
+let age = 23
 // FLASH FORWARD TO NEXT YEAR
 // reassign the value of age to be one greater than it was, because, we all get older
 
 // Code Here
-
+age = 24
 // Good news! We can live forever. Set your age to 999
 
 // Code Here
+age = 999
